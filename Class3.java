b0a9a60eab397ef942e2fefc293a9e3e65c55c43
Madeline Mason <mmason4@stu.jsu.edu>
2018-01-14 17:16:41 -0600

    Added Class3

---
+++ Class3.java
@@ -0,0 +1,16 @@
+public class Class2 {
+	private void f1() {
+		// Placeholder method (v1.0)
+	}
+	private void f2() {
+		//Placeholder method (v2.0)
+	}
+	private void f3() {
+		//Placeholder method (v3.0)
+	}
+}
+public class Class3 {
+	private void f4() {
+		//Placeholder method (v4.0)
+	}
+}
